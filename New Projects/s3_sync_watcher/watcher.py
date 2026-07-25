fccd2cd98f3048539ef13de79c89a18b9f9d4a08
headnutAi <demianwoerl@gmail.com>
2026-07-25 22:05:34 +0200

    Worket on watcher.py

--- New Projects/s3_sync_watcher/watcher.py
+++ New Projects/s3_sync_watcher/watcher.py
@@ -1,13 +1,21 @@
+import watchdog.events
 from watchdog.events import FileSystemEventHandler
+import logging
 
 class UploadHandler(FileSystemEventHandler):
     def __init__(self, uploader, prefix):
+        self.uploader = uploader
+        self.prefix = prefix
         # TODO: letzte Upload-Zeitpunkte pro Datei speichern (für Debouncing)
         pass
 
     def on_created(self, event):
-        pass
+        if not event.is_directory:
+
+            logging.info(f"File Created, {event.src_path}")
 
     def on_modified(self, event):
+        if not event.is_directory:
+            logging.info(f"File Modified, {event.src_path}")
         # TODO: hier auch Debounce-Check + Filter für .tmp/~$ Dateien
         pass
