b3db8e7d73f483b81c8507b4beb515e96450be3f
headnutAi <demianwoerl@gmail.com>
2026-08-10 19:34:45 +0200

    Worked on watcher.py

--- New Projects/s3_sync_watcher/watcher.py
+++ New Projects/s3_sync_watcher/watcher.py
@@ -7,6 +7,7 @@ class UploadHandler(FileSystemEventHandler):
     def __init__(self, uploader, prefix):
         self.uploader = uploader
         self.prefix = prefix
+        self.last_event_time = {}
         # TODO: letzte Upload-Zeitpunkte pro Datei speichern (für Debouncing)
         pass
 
@@ -22,22 +23,45 @@ class UploadHandler(FileSystemEventHandler):
 
         return False
 
+    def debounce_check(self, path, threshold = 1.0):
+
+        currentTime = time.time()
+
+        if path in self.last_event_time:
+            temp = self.last_event_time[path]
+
+            if currentTime - temp < threshold:
+                self.last_event_time[path] = currentTime
+                return True
+            else:
+                self.last_event_time[path] = currentTime
+                return False
+        else:
+            self.last_event_time[path] = currentTime
+            return False
     def on_created(self, event):
         if not event.is_directory:
 
             if self.should_ignore(event.src_path):
-                logging.info("ignoring {}".format(event.src_path))
+                logging.info("ignoring filtered {}".format(event.src_path))
                 return
+            if self.debounce_check(event.src_path):
+                logging.info("ignoring debounce{}".format(event.src_path))
+                return
+
             logging.info(f"File Created, {event.src_path}")
 
     def on_modified(self, event):
         if not event.is_directory:
 
             if self.should_ignore(event.src_path):
-                logging.info("ignoring {}".format(event.src_path))
+                logging.info("ignoring filtered{}".format(event.src_path))
                 return
+            if self.debounce_check(event.src_path):
+                logging.info("ignoring debounced{}".format(event.src_path))
+                return
+
             logging.info(f"File Modified, {event.src_path}")
-        # TODO: hier auch Debounce-Check + Filter für .tmp/~$ Dateien
         pass
 
 
