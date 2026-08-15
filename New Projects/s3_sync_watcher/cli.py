78a3fa6336ef95896f3a23e087115d1f263d6b23
headnutAi <demianwoerl@gmail.com>
2026-08-15 13:56:16 +0200

    Finished project

--- New Projects/s3_sync_watcher/cli.py
+++ New Projects/s3_sync_watcher/cli.py
@@ -5,15 +5,18 @@ from watchdog.observers import Observer
 import logging
 import click
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
 @click.command()
 @click.option('--folder', required=True, help='Folder to watch')
 @click.option('--bucket', required=True, help='Bucket to watch')
 @click.option('--prefix', required=True, help='Prefix to use')
 @click.option('--dry-run', is_flag=True, default=False, help='dry run')
 
+
 def main(folder, bucket, prefix, dry_run):
     logger = logging.getLogger()
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
 
     logger.info(f'Starting upload watcher mit den werten: {folder, bucket, prefix, dry_run}')
 
@@ -23,7 +26,14 @@ def main(folder, bucket, prefix, dry_run):
     observer = Observer()
     observer.schedule(handler, folder, recursive=False)
     observer.start()
-    # TODO: observer.join() oder Loop, damit das Programm nicht sofort endet
+
+    try:
+        observer.join()
+    except KeyboardInterrupt:
+        observer.stop()
+        observer.join()
+
+
 
 if __name__ == "__main__":
     main()
